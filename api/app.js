101a0145f25c36f98b9bfac2141c05d877b88a26
Shawn Randall <contactshawnrandall@gmail.com>
2026-05-28 09:15:26 -0400

    Remove static index.html so CDN falls through to /api/app function

--- api/app.js
+++ api/app.js
@@ -4,6 +4,6 @@ import { join } from "path";
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/html; charset=utf-8");
   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-  const html = readFileSync(join(process.cwd(), "index.html"), "utf8");
+  const html = readFileSync(join(process.cwd(), "src", "app.html"), "utf8");
   res.status(200).send(html);
 }
